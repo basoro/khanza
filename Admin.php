49a71cf23deb04bf80884d9dcf0ee01db19e321e
Basoro <dentix.id@gmail.com>
2023-11-18 13:43:16 +0800

    Update Admin.php

--- Admin.php
+++ Admin.php
@@ -295,8 +295,8 @@ class Admin extends AdminModule
 
             // Close archive and delete all unnecessary files
             $zip->close();
-            // unlink(BASE_DIR.'/tmp/latest.zip');
-            // deleteDir(BASE_DIR.'/tmp/update');
+            unlink(BASE_DIR.'/tmp/latest.zip');
+            deleteDir(BASE_DIR.'/tmp/update');
 
             $this->settings->reload();
 
